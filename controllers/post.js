c43221a41f79e3430372b8d6913ebfe599de6459
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-11-21 13:44:14 -0200

    Update file - update for update post

--- controllers/post.js
+++ controllers/post.js
@@ -16,10 +16,10 @@ module.exports = function(app){
 			conn.end();				
 			});			
 		},
-		create: function(req,res){
-			res.render('post/cadastro', {title : 'Post | Cadastro'});
+		form: function(req,res){
+			res.render('post/form', {form:'create', title : 'Cadastrar Postagem'});
 		},
-		post: function(req,res){
+		create: function(req,res){
 			var array = req.body;
 			var conn  = require('../config/mysql')();
 			var PostModel  = require('../DAO/Post')();
@@ -32,12 +32,38 @@ module.exports = function(app){
 						if (error) {
 							req.flash('erro','Erro ao Listar Postagens' + error);
 						}else{
-							res.render('post/consulta',{posts: results,title: 'Post | Consulta'});
+							res.render('post/consulta',{posts: results, title: 'Post | Consulta'});
 						}
 					conn.end();			
 					});
 				}
 			});			
+		},
+		update: function(req,res){
+			var id = req.params.id;
+			var array = req.body;
+			var conn  = require('../config/mysql')();
+			var PostModel  = require('../DAO/Post')();
+			var Post = new PostModel(conn);
+			Post.Read(id,function(error,results){
+				if (error) {
+					req.flash('erro','Erro ao Listar Postagens' + error);
+				}else{
+					res.render('post/form', {posts: results,form:'update', title : 'Atualizar Postagem'});
+					/*
+					Post.Update(array, function(error, results){
+						if (error) {
+							console.log(error);
+						}else{
+
+						}
+					});
+					*/
+					console.log(results);
+				}
+
+			})
+			//res.render('post/cadastro', {title : 'Atualizar Postagem'});
 		},
 		delete:function(req,res){
 			var id = req.params.id;
--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,8 @@ module.exports = function(app){
 	var post =  app.controllers.post;
 	app.route('/post').get(post.index);
 	app.route('/post/consulta').get(post.read);
-	app.route('/post/cadastro').get(post.create);
-	app.route('/post/cadastro').post(post.post);
+	app.route('/post/cadastro').get(post.form);
+	app.route('/post/create').post(post.create);
 	app.route('/post/remove/:id').get(post.delete);
+	app.route('/post/update/:id').get(post.update);
 }
