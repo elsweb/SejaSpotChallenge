fd8ed917fe376e36000b5c4900f3a9d9614f3648
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-11-21 00:21:55 -0200

    Update file - install dependency

--- config/express.js
+++ config/express.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('express-flash');
 module.exports = function(){	
 	var app = express();
 	app.set('view engine', 'jade');
 	app.set('views', './views');
-	app.use(express.static('./public'));	
+	app.use(express.static('./public'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(cookieParser('keyboard cat'));
+	app.use(session({ cookie: { maxAge: 60000 }}));
+	app.use(flash());
 	return app;
 }
