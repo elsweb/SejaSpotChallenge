05e0a2c3b54f23411c685c74cf6e4d22333d4fd6
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-11-21 00:20:24 -0200

    Update file - Read, Create and Delete

--- controllers/post.js
+++ controllers/post.js
@@ -2,19 +2,63 @@ module.exports = function(app){
 	var PostControll = {
 		index: function(req,res){
 			res.render('post/index', {title : 'Postagem'});
+		},		
+		read: function(req,res){
+			var conn  = require('../config/mysql')();
+			var PostModel  = require('../DAO/Post')();
+			var Post = new PostModel(conn);			
+			Post.ListAll(function(error, results){
+				if (error) {
+					req.flash('erro','Erro ao Listar Postagens' + error);
+				}else{
+					res.render('post/consulta',{posts: results,title: 'Post | Consulta'});
+				}
+			conn.end();				
+			});			
 		},
 		create: function(req,res){
 			res.render('post/cadastro', {title : 'Post | Cadastro'});
 		},
-		read: function(req,res){
+		post: function(req,res){
+			var array = req.body;
 			var conn  = require('../config/mysql')();
 			var PostModel  = require('../DAO/Post')();
-			var Post = new PostModel(conn);
-			Post.ListAll(function(error, results){
-				if (error) throw error;
-				res.render('post/consulta',{posts: results,title: 'Post | Consulta'});
-			})
-		}		
+			var Post = new PostModel(conn);			
+			Post.Create(array, function(error, results){
+				if (error) {
+					console.log(error);
+				}else{
+					Post.ListAll(function(error, results){
+						if (error) {
+							req.flash('erro','Erro ao Listar Postagens' + error);
+						}else{
+							res.render('post/consulta',{posts: results,title: 'Post | Consulta'});
+						}
+					conn.end();			
+					});
+				}
+			});			
+		},
+		delete:function(req,res){
+			var id = req.params.id;
+			var conn  = require('../config/mysql')();
+			var PostModel  = require('../DAO/Post')();
+			var Post = new PostModel(conn);			
+			Post.Delete(id,function(error, results){
+				if (error) {
+					console.log(error);
+				}else{
+					Post.ListAll(function(error, results){
+						if (error) {
+							req.flash('erro','Erro ao Listar Postagens' + error);
+						}else{
+							res.render('post/consulta',{posts: results,title: 'Post | Consulta'});
+						}
+					conn.end();			
+					});
+				}
+			});						
+		}				
 	}
 	return PostControll;
 }
--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,8 @@
 module.exports = function(app){
 	var post =  app.controllers.post;
 	app.route('/post').get(post.index);
-	app.route('/post/cadastro').get(post.create);	
 	app.route('/post/consulta').get(post.read);
+	app.route('/post/cadastro').get(post.create);
+	app.route('/post/cadastro').post(post.post);
+	app.route('/post/remove/:id').get(post.delete);
 }
