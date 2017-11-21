5ba02819ff8e88bb11a39b632152aea5173bfd19
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-11-21 00:18:52 -0200

    Update file - Create and Delete

--- DAO/Post.js
+++ DAO/Post.js
@@ -4,6 +4,17 @@ function PostDAO(conn){
 PostDAO.prototype.ListAll = function(callback){
 	this._conn.query('SELECT * FROM post',callback);
 }
+PostDAO.prototype.Create = function(array,callback){
+	var sql = "INSERT INTO post (post_title, post_content) VALUES ?";
+	var values = [[
+	  array.post_title,
+	  array.post_content
+	  ]];
+	this._conn.query(sql,[values],callback);
+}
+PostDAO.prototype.Delete = function(id,callback){
+	this._conn.query('DELETE FROM post WHERE post_id = "'+ id +'"', callback);
+}
 module.exports = function(){
 	return PostDAO;
 }
