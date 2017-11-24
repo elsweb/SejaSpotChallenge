bceea39a312c9de6833db5966bfcfb4ee3f80204
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-11-24 00:27:55 -0200

    API Create file - get and set post

---
+++ api-setpost.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var config = {
+    hostname: 'localhost',
+    port:3030,
+    path:'/post/create',
+    method: 'POST',
+    headers:{
+      'Accept':'application/json',
+      'Content-type':'application/json'
+    }
+};
+
+var client = http.request(config,function(res){
+  console.log(res.statusCode);
+  res.on('data',function(body){
+    console.log('json:' + body);
+  });  
+});
+
+var post = {
+  post_title : 'API Example',
+  post_content : 'Insert for API Example'
+}
+
+client.end(JSON.stringify(post));
