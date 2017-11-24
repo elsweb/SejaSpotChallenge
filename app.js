eaaf4b93af75e825d5d4fb387b6a90c7c9d9d34c
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-11-24 20:38:00 -0200

    Install - Automated testing on mocha and supertest

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ var load = require('express-load');
 //Config 
 load('controllers').then('DAO').then('routes').into(app); 
 app.listen(3030, function(){console.log('Server listening on port 3030!')}); 
+
+module.exports = app;
